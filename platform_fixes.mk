11655e3f3656a062f3247d3f3ad10c50dc44d679
TheDarkBug <adrianoliviero23@gmail.com>
2023-11-01 22:44:05 +0100

    Updated platform fixes

--- platform_fixes.mk
+++ platform_fixes.mk
@@ -1,60 +1,44 @@
 # platform (os) specific variable modifications
 
+
 ifeq ($(PLATFORM), Linux)
-	# PREFIX		= bin
-	# LIBDIR		= lib
-	# INCDIR		= include
-	# ETC_DIR		= /etc
-	# MANDIR		= share/man/man1
 	PLATFORM_ABBR = linux
 	ifeq ($(shell uname -o), Android)
-		CFLAGS				+= -D__ANDROID__
-		CFLAGS_DEBUG	+= -D__ANDROID__
-		# DEST_DIR				= /data/data/com.termux/files/usr
-		# ETC_DIR				= $(DEST_DIR)/etc
+		CFLAGS += -D__ANDROID__
+		CFLAGS_DEBUG += -D__ANDROID__
+		PREFIX_DIR = /data/data/com.termux/files/usr
 		PLATFORM_ABBR	= android
 	endif
 else ifeq ($(PLATFORM), Darwin)
-	# PREFIX		= local/bin
-	# LIBDIR		= local/lib
-	# INCDIR		= local/include
-	# ETC_DIR		= /etc
-	# MANDIR		= local/share/man/man1
+	USR_DIR = $(PREFIX_DIR)/usr/local
+	BIN_DIR = $(USR_DIR)/bin
+	LIB_DIR = $(USR_DIR)/lib
+	MAN_DIR = $(USR_DIR)/share/man/man1
+	INC_DIR = $(USR_DIR)/include
 	PLATFORM_ABBR = macos
 else ifeq ($(PLATFORM), FreeBSD)
-	CFLAGS		+= -D__FREEBSD__ -D__BSD__
+	CFLAGS += -D__FREEBSD__ -D__BSD__
 	CFLAGS_DEBUG += -D__FREEBSD__ -D__BSD__
-	# PREFIX		= bin
-	# LIBDIR		= lib
-	# INCDIR		= include
-	# ETC_DIR		= /etc
-	# MANDIR		= share/man/man1
 	PLATFORM_ABBR = freebsd
 else ifeq ($(PLATFORM), OpenBSD)
-	CFLAGS		+= -D__OPENBSD__ -D__BSD__
+	CFLAGS += -D__OPENBSD__ -D__BSD__
 	CFLAGS_DEBUG += -D__OPENBSD__ -D__BSD__
-	# PREFIX		= bin
-	# LIBDIR		= lib
-	# INCDIR		= include
-	# ETC_DIR		= /etc
-	# MANDIR		= share/man/man1
 	PLATFORM_ABBR = openbsd
 else ifeq ($(PLATFORM), Windows_NT)
-	CC					= gcc
-	# PREFIX			= "C:\Program Files"
-	# LIBDIR			=
-	# INCDIR			=
-	# MANDIR			=
-	RELEASE_SCRIPTS = release_scripts/*.ps1
+	CC = gcc
+	PREFIX_DIR = "C:\Program Files\uwufetch"
+	USR_DIR = $(PREFIX_DIR)\usr
+	ETC_DIR = $(PREFIX_DIR)\etc
+	BIN_DIR = $(USR_DIR)\bin
+	LIB_DIR = $(USR_DIR)\lib
+	MAN_DIR = $(USR_DIR)\share\man\man1
+	INC_DIR = $(USR_DIR)\include
+	RELEASE_SCRIPTS = release_scripts\*.ps1
 	PLATFORM_ABBR	= win64
-	EXT				= .exe
+	EXT = .exe
 else ifeq ($(PLATFORM), linux4win)
 	CC				= x86_64-w64-mingw32-gcc
 	CFLAGS			+= -D_WIN32
-	# PREFIX			=
-	# LIBDIR			=
-	# INCDIR		    =
-	# MANDIR			=
 	RELEASE_SCRIPTS = release_scripts/*.ps1
 	PLATFORM_ABBR	= win64
 	EXT				= .exe
